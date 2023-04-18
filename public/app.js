78de3e4b76b4f312b220dedf7514732bef24be13
cup-noodlehS <93570629+cup-noodlehS@users.noreply.github.com>
2023-04-18 17:21:46 +0800

    success

--- public/app.js
+++ public/app.js
@@ -3,5 +3,4 @@ const joketext = document.querySelector(".joketext");
 jokebtn.addEventListener("click", async () => {
   joke = await axios.get("https://v2.jokeapi.dev/joke/Any?type=single");
   joketext.innerHTML = joke.data.joke;
-  console.log(joke.data.joke);
 });
